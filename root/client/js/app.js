84e0ec3c039309ebf289118029e22bfbac67f3c1
peffjetzke <peffjetzke@gmail.com>
2021-08-11 22:48:25 -0700

    Update after moving API calls/keys to server

--- root/client/js/app.js
+++ root/client/js/app.js
@@ -1,28 +1,7 @@
 /*Imports*/
 //import { calcDays } from './js/calc'
-
-
-/*API Variables*/
-const geoURL = "http://api.geonames.org/searchJSON?"; //"http://api.geonames.org/searchJSON?q=london&username=demo"
-let geoHits = "&maxRows=1"; //&maxRows=1 if we need to change the returns
-const geoKey = "udacitypeff";
-
-const weatherURL = "https://api.weatherbit.io/v2.0/forecast/daily?"; //"https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=API_KEY&include=minutely"; 
-let weatherLat = ""; //&lat=
-let weatherLon = ""; //&lon=
-let units = "units=I"
-const weatherKey = "a705ae9dfa214e6b84d2b9425fc8d31b";
-
-//https://api.weatherbit.io/v2.0/forecast/daily?units=I&lat=47.60621&lon=-122.33207&key=a705ae9dfa214e6b84d2b9425fc8d31b
-
-const pixabayURL = "https://pixabay.com/api/?key=";
-let pixabaySearch = ""; //&q=searchterm
-let pixabayImage = "&image_type=photo"; //&image_type=imagetype
-const pixKey="22706625-414ce5abd7dca429882197d31";
-
-//Button event listeners
-// document.getElementById("generate").addEventListener("click", generatePage);
-// document.getElementById("reset").addEventListener("click", resetPage);
+//import {...} from './js/update'
+//import {...} from './js/validation'
 
 /*Create new trip button event*/
 function resetPage(e){
@@ -34,175 +13,41 @@ function generatePage(e){
     event.preventDefault()
     
     /*Start with data validation*/
-    if(document.getElementById("city_form").value == "")
-    {
-        alert("Please Enter a City");
-    }else if(document.getElementById("country_form").value == "")
-    {
-        alert("Please Enter a Country"); 
-    }else if(document.getElementById("start").value =="")
-    {
-        alert("Please select a start date."); 
-    }else if(document.getElementById("end").value =="")
-    {
-        alert("Please select an end date."); 
-    }else{
-        let city = document.getElementById("city_form").value;
-        let country = document.getElementById("country_form").value;
 
-        /*Date Variables*/
-        let startDate = new Date(document.getElementById("start").value);
-        let endDate = new Date(document.getElementById("end").value);
-        let today = new Date();
+    /*Assign variables from UI*/
+    let city = document.getElementById("city_form").value;
+    let country = document.getElementById("country_form").value;
+    /*Date Variables*/
+    let startDate = new Date(document.getElementById("start").value);
+    let endDate = new Date(document.getElementById("end").value);
+    let today = new Date();
 
-        console.log("Start: ", startDate);
-        console.log("End: ", endDate);
+    let duration = calcDays(endDate, startDate);
+    let forecastDays = calcDays(endDate, today);
 
-        /*Setup first API call to Geonames*/
-        let fullGeoURL = geoURL+"q="+city+"&country="+country+geoHits+"&username="+geoKey;
+    console.log("Start: ", startDate);
+    console.log("End: ", endDate);
 
-        /*Start chaining promises*/
-        getCoordinates(fullGeoURL)
-        .then(function(data){
-            //console.log(data); //returning data here
-            const lon = data.geonames[0].lng;
-            const lat = data.geonames[0].lat;
-            console.log("Coords: ", lat + ", " + lon);
-            document.getElementById("lat").innerHTML = "Lat: " + lon;
-            document.getElementById("lon").innerHTML = "Lon: " + lat;
-            let fullWeatherURL = weatherURL+units+"&lat="+lat+"&lon="+lon+"&key="+weatherKey;
-            console.log(fullWeatherURL);
-            const weatherData = getWeather(fullWeatherURL);
-            return weatherData;
-            //postData('/add', {temp: data.main.temp , date: today, feelings: feelings}) JUST COMMETED
-        })
-        .then((weatherData)=>{
-            console.log(weatherData);
+    /*Collect the form data in an object*/
+    let formData = {
+        city: city,
+        country: country,
+        forecastDays: forecastDays,
+        duration: duration};
 
-            /*Checking how far out the date is for the weather*/
-            let forecastDays = calcDays(endDate, today);
+    /*POST to server side with data from the form*/
+    postData('http://localhost:8081/add', formData)
+    .then(()=>{
+        const data = getData();
+    }).then((data)=>{
+        console.log(data);
+    }); //localhost:8081
 
-            console.log("Forcast Days: ", forecastDays);
-            if(forecastDays<=1){
-                let temp = weatherData.data[0].temp
-                let high = weatherData.data[0].high_temp;
-                let low = weatherData.data[0].low_temp;
-                let desc = weatherData.data[0].weather.description;
-                let icon = weatherData.data[0].weather.icon;
-
-                console.log("High: ", high);
-                console.log("Low: ", low);
-                console.log("Description : ", desc);
-                console.log("Current Weather: ", temp);
-                document.getElementById("current").innerHTML = "Curent: " + temp + " F";
-                document.getElementById("high").innerHTML = "High: " + high + " F";
-                document.getElementById("low").innerHTML = "Low: " + low + " F";
-                //document.getElementById("icon").imageSrc = "https://www.weatherbit.io/static/img/icons/" + icon + ".png";
-                document.getElementById("icon").setAttribute("src", "https://www.weatherbit.io/static/img/icons/" + icon + ".png");
-                console.log("https://www.weatherbit.io/static/img/icons/" + icon + ".png");
-                document.getElementById("description").innerHTML = desc;
-
-            }else if (forecastDays>=16) {
-                let high = weatherData.data[15].high_temp;
-                let low = weatherData.data[15].low_temp;
-                let desc = weatherData.data[15].weather.description;
-                let icon = weatherData.data[0].weather.icon;
-
-                document.getElementById("current").remove();
-                document.getElementById("high").innerHTML = "High: " + high + " F";
-                document.getElementById("low").innerHTML = "Low: " + low + " F";
-                document.getElementById("icon").setAttribute("src", "https://www.weatherbit.io/static/img/icons/" + icon + ".png");
-                document.getElementById("description").innerHTML = desc;
-            }else{
-                let high = weatherData.data[forecastDays].high_temp;
-                let low = weatherData.data[forecastDays].low_temp;
-                let desc = weatherData.data[forecastDays].weather.description;
-                let icon = weatherData.data[0].weather.icon;
-                
-                document.getElementById("current").remove();
-                document.getElementById("high").innerHTML = "High: " + high + " F";
-                document.getElementById("low").innerHTML = "Low: " + low + " F";
-                document.getElementById("icon").setAttribute("src", "https://www.weatherbit.io/static/img/icons/" + icon + ".png");
-                document.getElementById("description").innerHTML = desc;
-
-            }
-            
-            let city = weatherData.city_name
-            let country = weatherData.country_code
-
-            console.log("Location", city + " " + country);
-            document.getElementById("city").innerHTML = "City: " + city;
-            document.getElementById("country").innerHTML = "Country: " + country;
-
-            let pixFullURL = pixabayURL+pixKey+pixabayImage+"&q="+city;
-            console.log(pixFullURL); 
-            const imageSrc = getImage(pixFullURL);
-            console.log(imageSrc);
-            return imageSrc;
-            //postData('/add', {temp: data.main.temp , date: today, feelings: feelings}) JUST COMMETED
-        })
-        .then((imageSrc)=>{
-            console.log(imageSrc);
-            console.log("Array length: ", imageSrc.hits.length); //debug checking array length
-            console.log("Random element: ", Math.floor(Math.random()*imageSrc.hits.length)); //debug getting random array element
-
-            /*check for a no return, set default image if so*/
-            if(imageSrc.hits.length == 0)
-            {
-                document.body.style.backgroundImage = "url('./media/default.jpg')"; 
-            }else{
-                let i = Math.floor(Math.random()*imageSrc.hits.length);
-                let imgURL = imageSrc.hits[i].largeImageURL;     
-                console.log(imgURL);
-                document.body.style.backgroundImage = "url("+imgURL+")";
-            }
-
-            /*Calculate duration and update duration*/
-            let duration = calcDays(endDate, startDate);
-            document.getElementById("duration").innerHTML = "Trip Length: " + duration + " day(s)";
-            document.getElementById("itinerary").style.display = "flex";
-            document.getElementById("container").style.display = "none";
-            //postData('/add', {temp: data.main.temp , date: today, feelings: feelings}) JUST COMMETED
-        })
-        //Update the UI
-        //.then(()=> updateUI()); JUST COMMETED
-    }   
 }
 
 function calcDays(end, start){
     let days = Math.floor((end - start)/(1000*60*60*24));
     return days;
-}
-
-const getCoordinates = async (apiURL)=>{ //can this just be used generically? 
-    const res = await fetch(apiURL) //baseURL+zip+apiKey+units
-    try{
-        const data = await res.json();
-        return data;
-    } catch(error){
-        console.log("There was a problem", error);
-    }
-}
-
-/*Call Weather API data*/
-const getWeather = async (apiURL)=>{ //can this just be used generically? 
-    const res = await fetch(apiURL) //baseURL+zip+apiKey+units
-    try{
-        const data = await res.json();
-        return data;
-    } catch(error){
-        console.log("There was a problem", error);
-    }
-}
-
-const getImage = async (apiURL)=>{ //can this just be used generically? 
-    const res = await fetch(apiURL) //baseURL+zip+apiKey+units
-    try{
-        const data = await res.json();
-        return data;
-    } catch(error){
-        console.log("There was a problem", error);
-    }
 }
 
 /*Post the data, combine weather data from API with user data from UI*/
@@ -215,18 +60,33 @@ const postData = async (url = '', data = {})=>{
         },
         body: JSON.stringify(data),
     });
+    console.log("Response: ", response);
+    console.log("Response Body: ", response.body);
     try{
-        const allData = await response.json();
-        console.log(allData + "you made it here");
+        console.log(data);
+        //const allData = await response.json();
+        const allData = await response;
+        console.log("All Data Response:", allData.body);
         return allData;
     } catch(error){
         console.log("There was a problem", error);
     }
 }
 
+const getData = async () =>{ 
+    const request = await fetch('http://localhost:8081/all');
+    try {
+    const allData = await request.json();
+    console.log('allData ', allData);
+  }
+    catch(error) {
+      console.log("error", error);
+    }
+  }
+
 /*Update the UI with the returned Weather and feeling data*/
 const updateUI = async () => {
-    const request = await fetch('/all');
+    const request = await fetch('http://localhost:8080/all');
     try{
         const pageData = await request.json();
         console.log(pageData + "last debug"); //debug
--- root/server/server.js
+++ root/server/server.js
@@ -1,5 +1,10 @@
 /*Variables*/
 let travelData = {};
+let formData= {};
+let forecastData = {};
+
+const dotenv = require('dotenv');
+const fetch = require('node-fetch');
 
 /*Setup Server*/
 const express = require('express');
@@ -8,6 +13,7 @@ const app = express();
 /*Middleware*/
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { response } = require('express');
 
 /*bodyParser returns the depreciated warning. Including it instead of express to stay inline with rubric*/
 app.use(bodyParser.urlencoded({extended: false}));
@@ -27,34 +33,168 @@ function listening(){
   console.log(`Running on localhost: ${port}`);
 }
 
-/*Get*/
-// GET method route
+/*API Variables*/
+const geoURL = "http://api.geonames.org/searchJSON?";
+let geoHits = "&maxRows=1"; //&maxRows=1 if we need to change the returns
+const geoKey = process.env.GEO_KEY;
 
+const weatherURL = "https://api.weatherbit.io/v2.0/forecast/daily?"; 
+let weatherLat = ""; //&lat=
+let weatherLon = ""; //&lon=
+let units = "units=I"
+const weatherKey = process.env.BIT_KEY;
+
+const pixabayURL = "https://pixabay.com/api/?key=";
+let pixabaySearch = ""; //&q=searchterm
+let pixabayImage = "&image_type=photo"; //&image_type=imagetype
+const pixKey = process.env.PIX_KEY;
+
+let city = formData.city;
+let country = formData.country;
+let fullGeoURL = geoURL+"q="+city+"&country="+country+geoHits+"&username="+geoKey;
+
+/*API Calls*/
+const getCoordinates = async (fullGeoURL)=>{ //can this just be used generically?
+  const res = await fetch(fullGeoURL) 
+  try{
+      const data = await res.json();
+      return data;
+  } catch(error){
+      console.log("There was a problem", error);
+  }
+}
+
+const getWeather = async (fullWeatherURL)=>{ //can this just be used generically? 
+  const res = await fetch(fullWeatherURL) //baseURL+zip+apiKey+units
+  try{
+      const data = await res.json();
+      return data;
+  } catch(error){
+      console.log("There was a problem", error);
+  }
+}
+
+const getImage = async (pixFullURL)=>{ //can this just be used generically? 
+  const res = await fetch(pixFullURL) //baseURL+zip+apiKey+units
+  try{
+      const data = await res.json();
+      return data;
+  } catch(error){
+      console.log("There was a problem", error);
+  }
+}
+
+function callAPIs() {
+  getCoordinates()
+  .then(function(coordData){
+      //console.log(data); //returning data here
+      const lon = coordData.geonames[0].lng;
+      const lat = coordData.geonames[0].lat;
+      let fullWeatherURL = weatherURL+units+"&lat="+lat+"&lon="+lon+"&key="+weatherKey;
+      const weatherData = getWeather(fullWeatherURL);
+      return weatherData;
+  })
+  .then((weatherData)=>{
+    let forecastDays = formData.forecastDays;
+    if(forecastDays<=1){
+      let forecastData = {
+        temp: weatherData.data[0].temp,
+        high: weatherData.data[0].high_temp,
+        low: weatherData.data[0].low_temp,
+        desc: weatherData.data[0].weather.description,
+        icon: weatherData.data[0].weather.icon
+      }
+      forecastData.push(formData)
+      return formData;
+
+    }else if (forecastDays>=16) {
+      let forecastData = {
+        high: weatherData.data[15].high_temp,
+        low: weatherData.data[15].low_temp,
+        desc: weatherData.data[15].weather.description,
+        icon: weatherData.data[0].weather.icon
+      }
+      forecastData.push(formData)
+      return formData;
+
+  }else{
+    let forecastData = {
+        high: weatherData.data[forecastDays].high_temp,
+        low: weatherData.data[forecastDays].low_temp,
+        desc: weatherData.data[forecastDays].weather.description,
+        icon: weatherData.data[0].weather.icon,
+    }
+      forecastData.push(formData)
+      return formData;
+      //return forecastData;
+    }
+  })
+  .then((formData)=>{
+    let pixFullURL = pixabayURL+pixKey+pixabayImage+"&q="+formData.city;
+    const imageSrc = getImage(pixFullURL);
+    return imageSrc;
+  })
+  .then((imageSrc)=>{
+    if(imageSrc.hits.length == 0){
+      let imgsrc = "url('./media/default.jpg')"; 
+      //document.body.style.backgroundImage = "url('./media/default.jpg')";
+      imgsrc.push(formData);
+      return formData; 
+    }else{
+      let i = Math.floor(Math.random()*imageSrc.hits.length);
+      let imgURL = imageSrc.hits[i].largeImageURL;     
+      // console.log(imgURL);
+      let imgsrc = "url("+imgURL+")";
+      //document.body.style.backgroundImage = "url("+imgURL+")";
+      imgsrc.push(formData);
+      formData.push(travelData);
+      console.log(travelData);
+      return travelData;
+    }
+  })
+}
+
+console.log(travelData);
+
+/*Get*/
 function getData(req, res) {
     res.send(travelData);
   }
 
 app.get('/all', getData) 
 
-/*Post*/
-function postData(req, res) {
+function getFormData(req, res){
     let reqData = req.body;
 
-    console.log("Check request data: ", reqData);
+    formData["city"] = reqData.city;
+    formData["country"] = reqData.country;
+    formData["forecastDays"] = reqData.forecastDays;
+    formData["duration"] = reqData.duration;
+    res.send(callAPIs());
+}
 
-    travelData["duration"] = reqData.duration;
-    travelData["lat"] = reqData.lat;
-    travelData["lon"] = reqData.lon;
-    travelData["city"] = reqData.city;
-    travelData["country"] = reqData.country;
-    travelData["current"] = reqData.current;
-    travelData["high"] = reqData.high;
-    travelData["low"] = reqData.low;
-    travelData["desc"] = reqData.desc
-    travelData["imgsrc"] = reqData.imgsrc;
+/*Post*/
+// function postData(req, res) {
+//     let reqData = req.body;
 
-    res.send(travelData);
-    console.log(travelData + "server debug");
-  }
+//     console.log("Check request data: ", reqData);
 
-  app.post('/add', postData);
+//     travelData["duration"] = reqData.duration;
+//     travelData["lat"] = reqData.lat;
+//     travelData["lon"] = reqData.lon;
+//     travelData["city"] = reqData.city;
+//     travelData["country"] = reqData.country;
+//     travelData["current"] = reqData.current;
+//     travelData["high"] = reqData.high;
+//     travelData["low"] = reqData.low;
+//     travelData["desc"] = reqData.desc
+//     travelData["imgsrc"] = reqData.imgsrc;
+//     travelData["forecastDays"] = reqData.forecastDays;
+//     travelData["icon"] = reqData.icon;
+//     travelData["bgImg"] = reqData.bgImg;
+
+//     res.send(travelData);
+//     console.log(travelData + "server debug");
+//   }
+
+  app.post('/add', getFormData);
